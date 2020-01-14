78b1c23c6a8735823efe94da9e69a9aa6c22309f
jaganraj <jaganraj.g@krds.fr>
2020-01-14 17:30:50 +0530

    build

---
+++ build/precache-manifest.ec4d0e028ab3bca4c72d5ee3c4dd9a2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31046757a4d695e35c8031821ea651a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9623ad4f0adfa26c70e7",
+    "url": "/static/css/main.0a973b0a.chunk.css"
+  },
+  {
+    "revision": "e5f63f8d90fa9d674d5d",
+    "url": "/static/js/2.0a868b86.chunk.js"
+  },
+  {
+    "revision": "9623ad4f0adfa26c70e7",
+    "url": "/static/js/main.d327f1af.chunk.js"
+  },
+  {
+    "revision": "92a8f6c2e0c94cc10734",
+    "url": "/static/js/runtime-main.2fbbb0fa.js"
+  }
+]);
